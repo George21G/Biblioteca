e98640a7f47ff5740c89f4cd3b9d89aae4e2d39b
Jorge Robles SML <150305777+George21G@users.noreply.github.com>
2025-06-26 12:22:24 -0500

    implementación de  el archivo indice de servicios
    centraliza todas las exportaciones de los servicios

---
+++ resources/js/services/index.ts
@@ -0,0 +1,9 @@
+// Exportar todos los servicios
+export { librosService } from './librosService';
+export { usuariosService } from './usuariosService';
+export { institucionesService } from './institucionesService';
+export { prestamosService } from './prestamosService';
+export { authService } from './authService';
+
+// Exportar la instancia de API
+export { default as api } from '../lib/api'; 
